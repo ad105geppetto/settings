4b152e50116269911a1d0828fec8670258947e29
ad105geppetto <ad105geppetto@gmail.com>
2023-05-02 01:31:58 +0900

    init: 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>간단한 리스트 페이지</h1>
+      <ul>
+        <li>리스트 항목 1</li>
+        <li>리스트 항목 2</li>
+        <li>리스트 항목 3</li>
+        <li>리스트 항목 4</li>
+        <li>리스트 항목 5</li>
+        <li>리스트 항목 1</li>
+        <li>리스트 항목 2</li>
+        <li>리스트 항목 3</li>
+        <li>리스트 항목 4</li>
+        <li>리스트 항목 5</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
